6b4de434b1178690805f076142b737540c53c219
Linh Hong <lilogia@gmail.com>
2016-10-14 10:36:04 +0700

    update install systinet 11 script

--- hp/install/sys11.00/run.bat
+++ hp/install/sys11.00/run.bat
@@ -1,1 +1,1 @@
-call java -jar C:\Temp\systinet\hpe-systinet-11.00.jar -u C:\git\linh\worknote\hp\install\sys11.00\linhzbook15.postgres.properties -i C:\server\s11
+call java -jar C:\Temp\systinet\hpe-systinet-11.00.jar -u C:\git\linh\worknote\hp\install\sys11.00\hpswvm234189.postgres.properties -i C:\server\s1100
